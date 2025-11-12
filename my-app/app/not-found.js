8dfa486c926680c04f41f108417636dccccf6152
RaviTesla1 <ravibatalvi55@gmail.com>
2025-11-12 14:38:08 +0530

    Not found page done

---
+++ my-app/app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function noTfound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <p>The page you are looking for is not there !</p>
+    </div>
+  )
+}
+
+ 
